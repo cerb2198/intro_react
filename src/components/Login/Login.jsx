e56aa5dc39b60ec0b7c23d0eddde3e004f8d6bef
carlos eduardo <cerb2198@gmail.com>
2022-04-24 14:48:42 -0500

    Vista condicional

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-
-import "./login.css"; //Así se cargan los estilos.
+  
+import styles from "./login.module.css"; //Así se cargan los estilos.
 
 import imgUser from "../../resources/User_Circle.png"; //Así cargo yo las imagenes en imgUser puedes colocar cualquier nombre.
 
@@ -11,6 +11,9 @@ const Login = () => {
     contrasena:""
   });
 
+  //Nuestro estado para mostrar o no un mensaje:
+  const [mostrar, setMostrar] = useState(false);
+
   const cambioFormulario = (e) => {
     setDatos({
       ...datos,
@@ -18,16 +21,22 @@ const Login = () => {
     )
   }
 
-  const enviarDatos = (e) => {
+  const mostrarMensaje = (e) => {
     e.preventDefault();
-    console.log("Enviando estos datos...", datos);
+
+    if(mostrar===false){
+      setMostrar(true);
+    }else{
+      setMostrar(false);
+    }
+
   };
 
   return (
     //Tengo un elemento HTML tipo div, este yo lo uso para que sea el contenedor, este div ocupara todo el ancho y largo de la pantalla.
     //Para mirar el css metete al archivo login.css que esta en la carpeta Login.
-    <div className="contenedor">
-      <form className="form__login">
+    <div className={styles.contenedor}>
+      <form className={styles.form__login}>
         <img className="imagen" src={imgUser}></img> {/*Así se vincula la imagen:<img src={nombre_del_import}></img>*/}
         {/*Si quieres puedes darles clases a los elementos de acá para abajo, por si quieres estilizarlos mas...*/}
         {"Bienvenido" }
@@ -35,9 +44,24 @@ const Login = () => {
         <input name="correo" type="text" onChange={cambioFormulario}></input>
         <label>Contraseña</label>
         <input name="contrasena" type="password"  onChange={cambioFormulario}></input>
-        <button onClick={enviarDatos}>Enviar</button>
+        <button onClick={mostrarMensaje}>Enviar</button>
       </form>
 
+      {/*FORMATO DE VISTA CONDICIONAL, ES EQUIVALENTE A UN IF ELSE*/}
+      {mostrar ?
+        <form className={styles.form__login}>
+          <img className="imagen" src={imgUser}></img> {/*Así se vincula la imagen:<img src={nombre_del_import}></img>*/}
+          {/*Si quieres puedes darles clases a los elementos de acá para abajo, por si quieres estilizarlos mas...*/}
+          {"Bienvenido" }
+          <label>Correo</label>
+          <input name="correo" type="text" onChange={cambioFormulario}></input>
+          <label>Contraseña</label>
+          <input name="contrasena" type="password"  onChange={cambioFormulario}></input>
+          <button onClick={mostrarMensaje}>Enviar</button>
+        </form>
+        :
+        null
+      }
     </div> 
     
   );
